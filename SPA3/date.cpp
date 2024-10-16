851716cb1fc8bd1bb80ea9d7ca5a0f402854f050
Leigh <makenna.goetsch@gmail.com>
2024-10-16 14:22:50 -0500

    date contains no errors

--- SPA3/date.cpp
+++ SPA3/date.cpp
@@ -12,10 +12,11 @@
 using namespace std;
 
 Date::Date(int month, int day, int year)
-    : _month{month}, _day{day}, _year{year}
-{ }
+  : _month{month}, _day{day}, _year{year} {
+}
 
-Date::Date() : _month{0}, _day{0}, _year{0} { }
+Date::Date() : _month{0}, _day{0}, _year{0} {
+}
 
 int Date::month() const {
   return _month;
@@ -29,28 +30,22 @@ int Date::year() const {
   return _year;
 }
 
-string Date::to_string() const
-{
+string Date::to_string() const {
   // ostringstreams provide an cout-like interface but store the results
   //   in a std:string instead of files; this convert data into a string
   ostringstream formatted;
-  if ( _month < 10 )
+  if (_month < 10)
     formatted << "0";
   formatted << _month << "/";
-  if ( _day < 10 )
+  if (_day < 10)
     formatted << "0";
   formatted << _day << "/" << _year;
 
   return formatted.str();
 }
 
-bool earlier(Date d1, Date d2)
-{
-  // TODO: implement this; d1 is earlier than d2 if d1's year is earlier.
-  //       If the years are the same, check months, and if the months
-  //       are the same check the days.
-
-
+bool Date::earlier(Date d1, Date d2) {
+  return d1.year() < d2.year() ? true : d1.month() < d2.month() ? true : d1.day() < d2.day();
 }
 
 // TODO: if useful to your design, add code to check for valid dates
