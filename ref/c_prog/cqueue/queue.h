68149ff3717b18f9ce9d1cbd38447aafa05cc68b
Leigh <makenna.goetsch@gmail.com>
2024-12-02 21:20:26 -0600

    queue

---
+++ ref/c_prog/cqueue/queue.h
@@ -0,0 +1,16 @@
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+#define WORDLENGTH 32
+
+struct QNode;
+
+typedef struct {
+  struct QNode *head;
+  struct QNode **tail;
+  } Queue;
+
+  void initQueue(Queue *queue);
+  void offer(Queue *queue, char * word);
+  char * poll(Queue *queue);
+  void clearQueue(Queue *queue);
