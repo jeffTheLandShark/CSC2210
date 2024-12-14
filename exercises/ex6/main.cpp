14a33713db6f880c3945ec292c2d928c3f5afa55
Leigh <makenna.goetsch@gmail.com>
2024-12-13 23:29:52 -0600

    ex6!

--- exercises/ex6/main.cpp
+++ exercises/ex6/main.cpp
@@ -4,12 +4,27 @@
  * Exercise   : ex6
  */
 
-
+#include <iostream>
 
 int main() {
-    float imput;
-    std::cout << 
+  float input;
+  std::cout << "Enter a floating-point number: ";
+  std::cin >> input;
 
+  uint32_t *bits_pointer = reinterpret_cast<uint32_t *>(&input);
+  uint32_t value = *bits_pointer;
 
-    return 0;
+  std::cout << "Input: " << input << ", hex 0x";
+  ;
+  std::cout << std::hex << value << std::endl;
+
+  uint32_t matissa = value & 0x007FFFFF;
+  bool overHalf = (matissa & 0x00400000) && (matissa & 0x003FFFFF);
+
+  if (overHalf) {
+    std::cout << "mantissa is greater than a half" << std::endl;
+  } else {
+    std::cout << "mantissa is a half or less" << std::endl;
+  }
+  return 0;
 }
